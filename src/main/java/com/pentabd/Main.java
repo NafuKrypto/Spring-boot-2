7d468046d201b18e894dc67e3eac984fd7f71ab5
NafuKrypto <nafisaalam131@gmail.com>
2023-09-21 14:43:47 +0600

    Json Java with class

--- src/main/java/com/pentabd/Main.java
+++ src/main/java/com/pentabd/Main.java
@@ -2,12 +2,11 @@ package com.pentabd;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @SpringBootApplication
 //@ComponentScan(basePackages = "com.pentabd")
@@ -31,7 +30,46 @@ public class Main {
         return  new Greeting("My name is khane hello");
     }
 
+    @GetMapping("/greetingTwo")
+    public GreetResponse greetingTwo(){
+        return  new GreetResponse("My name is nafisa hello");
+    }
+
+
     record Greeting (String greets){
     //class
     }
+
+    class GreetResponse{
+        private final String greet;
+        GreetResponse(String greet){
+
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetResponse that = (GreetResponse) o;
+            return Objects.equals(greet, that.greet);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(greet);
+        }
+    }
+
 }
